08157c565a4dba2122c1a2442efc7a52cbb92bf3
satyam singh <ss4078017@gmail.com>
2022-12-18 20:52:27 +0530

    Channel header created

---
+++ src/components/Content/Content.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './ContentStyle.css'
+
+export default function Content(){
+    return (
+        <>
+            <div className="content">
+                Content 
+            </div>
+        </>
+    )
+}
