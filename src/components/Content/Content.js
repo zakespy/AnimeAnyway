71d111a0647a66df2eeac2204636c466adff666d
RakshitPatel910 <86058505+RakshitPatel910@users.noreply.github.com>
2022-12-20 13:01:35 +0530

    mian body container divided

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import './ContentStyle.css'
 
 export default function Content(){
+
+    const [volumes, setvolumes] = useState([
+        'url1', 'url2', 'url3', 'url4', 'url5', 'url6', 'url7', 'url8', 'url9', 'url10'
+    ])
+
     return (
         <>
-            <div className="content">
-                Content 
+            <div className="main-container">
+                
+                <div className="link-container">
+
+                </div>
+
+                <div className="info-container">
+
+                </div>
+
             </div>
         </>
     )
