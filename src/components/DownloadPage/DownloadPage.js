ca08501a67e5ec2a78243e3cc02790e60ec045d9
RakshitPatel910 <86058505+RakshitPatel910@users.noreply.github.com>
2022-12-26 11:04:22 +0530

    download url added

--- src/components/DownloadPage/DownloadPage.js
+++ src/components/DownloadPage/DownloadPage.js
@@ -11,13 +11,13 @@ export default function Content(){
     const location = useLocation();
     const url = location.state?.url;
     const { volNum } = useParams();
-    
+    console.log(url)
 
     return (
         <>
             <div className="download-container">
                 Download { volNum } of COTE
-                <a href={url}>
+                <a href={url} target="_blank" >
                     <button className="download-button">
                         Download
                         <DownloadIcon sx={{ color: "black", margin: "0 0 0 1rem" }} fontSize="large" />
