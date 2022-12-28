c6b6948d241ab1816f2a4c6f791c8f3f9caac49a
Mihir6365 <savla.mp@somaiya.edu>
2022-12-28 23:14:19 +0530

    some minor changes

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+import { volumeData } from '../../constants/volumeObj.js';
+import './ContentStyle.css';
 
-import { volumeData } from '../../constants/volumeObj.js'
+export default function Content() {
 
-import './ContentStyle.css'
-
-export default function Content(){
-
-    const [volumes, setvolumes] = useState( volumeData.slice(0).reverse() );
-    console.log(volumes)
+    const [volumes, setvolumes] = useState(volumeData.slice(0).reverse());
+    // console.log(volumes)
     // const [volumes, setvolumes] = useState({
     //     vol1: "",
     //     vol2: "",
@@ -18,21 +16,26 @@ export default function Content(){
     return (
         <>
             <div className="main-container">
-                
+
                 <div className="link-container">
                     {
-                        volumes.map( (vol) => (
-                            <Link to={`/download/${"Volume-"+vol.volumeNumber}`} state={{ url: vol.driveURL }} className="volume-url" style={{ background: `${volumeData.indexOf(vol) % 2 ? "rgba(245, 245, 245, 0.8)" : "rgba(255, 255, 255, 0.8)"}` }} key={vol.volumeNumber}>
-                                Volume {vol.volumeNumber}
+                        volumes.map((vol) => (
+                            <Link to={`/download/${vol.name}`} state={{ url: vol.driveURL }} className="volume-url" style={{ background: `${volumeData.indexOf(vol) % 2 ? "rgba(245, 245, 245, 0.8)" : "rgba(255, 255, 255, 0.8)"}` }} key={vol.volumeNumber}>
+                                {vol.name}
                                 {/* <hr></hr> */}
                                 {/* <a href="./" download={"../../cl2-header.png"}>download</a> */}
                             </Link>
-                         ) )
-                    }                    
+                        ))
+                    }
                 </div>
 
                 <div className="info-container">
+                    <strong>
 
+                        Volume 0 will be translated by January 5th. <br /> <br />
+                        Year 2 Volume 9 is estimated to release on february 25th, and trial version will be released early in february.
+                        Consider subscribing to the channel for more updates!
+                    </strong>
                 </div>
 
             </div>
--- src/components/DownloadPage/DownloadPage.js
+++ src/components/DownloadPage/DownloadPage.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
-import { Button } from "@mui/material";
 import DownloadIcon from '@mui/icons-material/Download';
+import { Button } from "@mui/material";
 
-import './DownloadPage.css'
+import './DownloadPage.css';
 
-export default function Content(){
+export default function Content() {
 
     const location = useLocation();
     const url = location.state?.url;
-    const { volNum } = useParams();
+    const { name } = useParams();
     console.log(url)
 
     return (
         <>
             <div className="download-container">
-                Download { volNum } of COTE
+                Download {name} of COTE
                 <a href={url} target="_blank" rel="noreferrer" >
                     <button className="download-button">
                         Download
