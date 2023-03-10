0edf57c07ffba3d928b1f6c31b577255977d7c98
Mihir6365 <savla.mp@somaiya.edu>
2023-03-10 20:48:52 +0530

    minor adjustments

--- src/components/BuyMeCoffee/BuyMeCoffee.js
+++ src/components/BuyMeCoffee/BuyMeCoffee.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import './buyMeCoffeeStyle.css'
+import React from 'react';
+import './buyMeCoffeeStyle.css';
 
-export default function BuyMeCoffee(){
-    return (
-      <>
-        <div className="coffee">
+export default function BuyMeCoffee() {
+  return (
+    <>
+      <div className="coffee">
+        <strong>
           <a href="https://www.buymeacoffee.com/animeanyway11" target="_blank">
-            <h2 className='text'>Buy Me A Coffee</h2>
+            <p className='text'>Buy Me A Coffee</p>
           </a>
-        </div>
-      </>
-    );
+        </strong>
+      </div>
+    </>
+  );
 }
