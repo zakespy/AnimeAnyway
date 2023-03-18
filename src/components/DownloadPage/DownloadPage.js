918112479eab64f415733996b5bca2aadeeb750d
Mihir6365 <savla.mp@somaiya.edu>
2023-03-18 16:32:55 +0530

    bug fix

--- src/components/DownloadPage/DownloadPage.js
+++ src/components/DownloadPage/DownloadPage.js
@@ -17,11 +17,9 @@ export default function Content() {
 
   const pdfDownload = () => {
     const pdf = document.getElementById("pdf-btn");
-    pdf.onClick = () => { return }
-    pdf.style.backgroundColor = "rgba(255, 238, 238, 0.9)";
-    pdf.style.color = "black";
-    pdf.style.border = "none";
-    pdf.innerText = "Downloading, " + pdfprogress + "%";
+    const pdf_percent = document.getElementById("pdf_percent");
+    pdf_percent.style.display = "inline-block"
+    pdf.style.display = "none"
     const pdf_len = translatedVolume[volume_index].pdf_size * 1024;
     fetch(`/assets/file/${translatedVolume[volume_index].name}.pdf`).then(
       response => {
@@ -36,6 +34,7 @@ export default function Content() {
                   .then((progressEvent) => {
                     if (progressEvent.done) {
                       controller.close();
+                      setpdfprogress(0)
                       return;
                     }
                     loaded += progressEvent.value.byteLength;
@@ -56,8 +55,10 @@ export default function Content() {
         alink.href = fileURL;
         alink.download = `${translatedVolume[volume_index].name}.pdf`;
         alink.click();
-        pdf.innerText = `Download Complete!`
+        pdf_percent.innerText = `Download Complete!`
         setTimeout(() => {
+          pdf.style.display = "inline-block"
+          pdf_percent.style.display = "none"
           pdf.style.backgroundColor = "rgba(255, 101, 101, 0.9)";
           pdf.style.color = "white";
           pdf.innerText = "Download pdf again?"
@@ -68,13 +69,10 @@ export default function Content() {
   };
 
   const epubDownload = () => {
-
     const epub = document.getElementById("epub-btn");
-    epub.onClick = () => { return }
-    epub.style.backgroundColor = "rgba(255, 238, 238, 0.9)";
-    epub.style.color = "black";
-    epub.style.border = "none";
-    epub.innerText = "Downloading, " + epubprogress + "%";
+    const epub_percent = document.getElementById("epub_percent");
+    epub_percent.style.display = "inline-block"
+    epub.style.display = ('none')
     const epub_len = translatedVolume[volume_index].epub_size * 1024;
     fetch(`/assets/file/${translatedVolume[volume_index].name}.epub`).then(
       response => {
@@ -89,11 +87,11 @@ export default function Content() {
                   .then((progressEvent) => {
                     if (progressEvent.done) {
                       controller.close();
+                      setepubprogress(0)
                       return;
                     }
                     loaded += progressEvent.value.byteLength;
                     var percentage = (Math.round(loaded / epub_len * 100));
-                    console.log(percentage)
                     setepubprogress(percentage)
                     controller.enqueue(progressEvent.value);
                     read();
@@ -110,8 +108,10 @@ export default function Content() {
         alink.href = fileURL;
         alink.download = `${translatedVolume[volume_index].name}.epub`;
         alink.click();
-        epub.innerText = `Download Complete!`
+        epub_percent.innerText = `Download Complete!`
         setTimeout(() => {
+          epub_percent.style.display = "none"
+          epub.style.display = "inline-block"
           epub.style.backgroundColor = "rgba(255, 101, 101, 0.9)";
           epub.style.color = "white";
           epub.innerText = "Download epub again?"
@@ -129,19 +129,20 @@ export default function Content() {
       <div className="download-container">
         <h1>Download {translatedVolume[volume_index].name} of Classroom Of The Elite</h1>
         <button className="download-button" id="pdf-btn" onClick={pdfDownload}>
-          Download as .pdf
-          <DownloadIcon
+          Download as .pdf <DownloadIcon
             sx={{ color: "white", margin: "0 0 0 1rem" }}
             fontSize="large"
           />
         </button>
+        <p id="pdf_percent">Downloading, {pdfprogress}%</p>
+
         <button className="download-button" id="epub-btn" onClick={epubDownload}>
-          Download as .epub
-          <DownloadIcon
+          Download as .epub <DownloadIcon
             sx={{ color: "white", margin: "0 0 0 1rem" }}
             fontSize="large"
           />
         </button>
+        <p id="epub_percent">Downloading, {epubprogress}%</p>
       </div>
     </>
   );
